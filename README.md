6c1b43557a8b649482daa5ed8826d128b4432c52
tburkholz <tburkholz@gasag.de>
2020-10-28 23:39:29 +0100

    Simple boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MLB-DeepLearning-Project
 Win predictions for MLB Games using deeplearning models
+
+# **Installed Packages**
+* Pandas (pip install pandas)
+* Xlrd (pip install xlrd)
--- TeamMerge.py
+++ TeamMerge.py
@@ -1,11 +1,12 @@
-import pandas as pd
+import xlrd
 from pathlib import Path
 
-data_folder = Path("../MLP_Win_Prediction/Data")
+data_folder = Path("./Data")
 
 base_data_file = data_folder / "base_data.xlsx"
 
-pd.read_excel(base_data_file)
+base_data_workbook = xlrd.open_workbook(base_data_file)
 
+base_data_worksheet = base_data_workbook.sheet_by_name("Teams")
 
-
+print(base_data_worksheet.cell_value(2, 3))
