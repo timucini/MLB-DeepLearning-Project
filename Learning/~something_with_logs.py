49bcd40f74d768d71c2f8a87e8c7fd8f493d1f01
DonBrezz <donbrezz@outlook.de>
2021-01-11 11:05:07 +0100

    update

--- Learning/~something_with_logs.py
+++ Learning/~something_with_logs.py
@@ -3,6 +3,7 @@ import numpy as np
 import random as rd
 from pathlib import Path
 from datetime import datetime as dt
+from tensorflow.keras.models import load_model
 
 def loadLog(name):
     df = pd.read_csv(path/'Logs'/name, index_col=False)
@@ -30,9 +31,11 @@ def changeIdentifier(log):
                 value = value+(p+1)*31*ord(char)*113*len(string)*271
             return value
         identifier = 0
+        b = 0
         for string in sample:
+            b = b+ord(string[0])
             identifier = identifier + string2int(string)
-        b = sample[0][0].upper()+str(len(sample)).zfill(2)+'D'
+        b = chr(b//len(sample))+str(len(sample)).zfill(2)+'D'
         return (b+str(identifier).zfill(16-len(b)))[:16]
     print('Old:')
     print(log[['predictors','identifier']])
@@ -51,12 +54,29 @@ def dropDuplicates(log):
     dups = dups.drop_duplicates(subset=['identifier'], keep='last')
     print(dups)
     return log.drop(dups.index)
+def appendModels(log):
+    models = []
+    for model in (path/'Models').glob('*.h5'):
+        models.append(model.name)
+    models = pd.DataFrame({'model':models})
+    models['identifier'] = models['model'].str.replace('.h5','')
+    return pd.merge(log, models, how='left', on='identifier')
+def renameModels(log):
+    log = log[['identifier','model']].dropna()
+    log = log[log['identifier']!=log['model'].str.replace('.h5','')]
+    rename = log['identifier'].tolist()
+    old = log['model'].tolist()
+    for index, model in enumerate(old):
+        load_model(path/'Models'/model).save(path/'New Models'/(rename[index]+'.h5'))
+
 
 path = Path(__file__).parent.absolute()/'Deep Training'
 pre = loadLog('None_predictors_log.csv')
+#pre = appendModels(pre)
 par = loadLog('None_parameter_log.csv')
 pre = changeIdentifier(pre)
 pre = dropDuplicates(pre)
+#renameModels(pre)
 par = changeIdentifier(par)
 pre.to_csv(path/'Logs'/'None_predictors_log.csv', index=False)
 par.to_csv(path/'Logs'/'None_parameter_log.csv', index=False)
